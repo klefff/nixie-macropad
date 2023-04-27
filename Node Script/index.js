2a79d4382def12febbc68a82f58a37cf025bb9d5
Luey <58355982+lskyinfinity@users.noreply.github.com>
2023-04-27 17:41:34 +1000

    completely moved tube refresh to qmk

--- Node Script/index.js
+++ Node Script/index.js
@@ -52,17 +52,6 @@ function updateKeyboard() {
     }
     else {
         // The target keyboard is found
-
-        //Increment Refresh Timer
-        // tubeRefreshTimer = tubeRefreshTimer + KEYBOARD_UPDATE_TIME;
-        // if (tubeRefreshTimer == TUBE_REFRESH_INTERVAL)
-        // {
-        //     keyboard.write([0, 1, 34, cur_vol, 2]);
-        //     if (cur_mute)
-        //         keyboard.write([0, 1,34, cur_vol, 1]);
-        //     tubeRefreshTimer = 0;
-        // }
-        // Check if the mute status has changed. This take priority over volume display 
         if (cur_mute != prev_mute)
             {
                 if (cur_mute)
